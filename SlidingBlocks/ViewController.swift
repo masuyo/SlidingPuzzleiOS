0ba324c79ecdbabfc6110ee5515078df9e881f2f
aimiliona <aimiliona@aimilionas-mac.local>
2019-03-25 17:42:45 +0100

    Initial Commit

---
+++ SlidingBlocks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlidingBlocks
+//
+//  Created by aimiliona on 2019. 03. 25..
+//  Copyright © 2019. aimiliona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SlidingBlocksTests/SlidingBlocksTests.swift
@@ -0,0 +1,34 @@
+//
+//  SlidingBlocksTests.swift
+//  SlidingBlocksTests
+//
+//  Created by aimiliona on 2019. 03. 25..
+//  Copyright © 2019. aimiliona. All rights reserved.
+//
+
+import XCTest
+@testable import SlidingBlocks
+
+class SlidingBlocksTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
