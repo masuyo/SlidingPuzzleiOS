0788f662f5f40a1d6a082bd95a945e13468182bc
aimiliona <aimiliona@aimilionas-mac.local>
2019-03-25 18:49:19 +0100

    draggable rect

--- SlidingBlocks/ViewController.swift
+++ SlidingBlocks/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var rect: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        rect = UIView(frame: CGRect(x: 30, y: 30, width: 50, height: 50))
+        rect.backgroundColor = UIColor.orange
+        
+        self.view.addSubview(rect)
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector (self.pan))
+        rect.addGestureRecognizer(pan)
     }
-
+    
+    @objc func pan(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            print("begin")
+        case .ended:
+            print("end")
+        case .changed:
+            self.rect.center = sender.location(ofTouch: 0, in: self.view)
+        default:
+            print("hello")
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
 }
 
