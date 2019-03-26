0b6c2412857e3e856c0237dcf70c5099d3fe9c83
aimiliona <aimiliona@aimilionas-mac.local>
2019-03-26 17:39:38 +0100

    models added

---
+++ SlidingBlocks/Models/Block.swift
@@ -0,0 +1,13 @@
+//
+//  Block.swift
+//  SlidingBlocks
+//
+//  Created by aimiliona on 2019. 03. 26..
+//  Copyright © 2019. aimiliona. All rights reserved.
+//
+
+import Foundation
+
+class Block {
+    
+}
---
+++ SlidingBlocks/Models/Board.swift
@@ -0,0 +1,13 @@
+//
+//  Board.swift
+//  SlidingBlocks
+//
+//  Created by aimiliona on 2019. 03. 26..
+//  Copyright © 2019. aimiliona. All rights reserved.
+//
+
+import Foundation
+
+class Board {
+    
+}
